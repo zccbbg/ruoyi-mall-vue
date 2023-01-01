1f066fa28705a374c1736f721055b2f4d26d8a70
zhaochencheng <zccbbg@qq.com>
2023-01-02 00:00:27 +0800

    商品、sku插入基本完成

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,147 +1,165 @@
-<template lang="pug">
-  .home-wrapper
-    mixin info
-      el-popover.ml5(content="功能开发中" placement="top" trigger="hover")
-        i.el-icon-warning-outline(slot="reference")
-    el-card(style="margin: 20px 20px; font-size: 14px")
-      div(slot="header")
-        span 工作台
-      .first
-        img(
-          alt=""
-          :src="avatar"
-          style="width: 50px; height: 50px; float:left; margin-right:10px;border-radius:50%"
-        )
-        p(style="font-size:16px;margin-bottom:8px") {{name}}，{{hello}}
-        p(style="font-size:12px;color:rgb(185, 181, 189)") 今天是：{{nowTime}}
-      .first
-        ul
-          li.li(style="width: 33%") 访客数
-            +info
-          li.li(style="width: 33%") 支付买家数
-            +info
-          li.li(style="width: 33%") 浏览量
-            +info
-        ul
-          li.da(style="width: 33%")
-            router-link(to="/basic/warehouse") 2,910
-          li.da(style="width: 33%")
-            router-link(to="/basic/area") 56
-          li.da(style="width: 33%")
-            router-link(to="/basic/rack") 21,085
-    el-row.pl20.pr20(:gutter="10")
-      el-col(:span="8")
-        el-card(shadow="always" style="padding-bottom: 20px")
-          div(slot="header")
-            span 售后
-          ul
-            li.li(style="width: 50%") 待处理
-              +info
-            li.li(style="width: 50%") 处理中
-              +info
-          ul
-            li.da(style="width: 50%") 2
-            li.da(style="width: 50%") 1
-      el-col(:span="16")
-        el-card(shadow="always" style="padding-bottom: 20px")
-          div(slot="header")
-            span 订单
-          div
-            ul
-              li.li 未发订单数
-                +info
-              li.li 未揽件有退款
-                +info
-              li.li 发货超时
-                +info
-              li.li 物流预警
-                +info
-            ul
-              li.da
-                router-link(to="/relation/customer") 50
-              li.da
-                router-link(to="/relation/supplier") 0
-              li.da
-                router-link(to="/relation/carrier") 4
-              li.da
-                router-link(to="/item") 3
-    el-row.pl20.pr20(:gutter="10")
-      el-col(:span="16")
-        el-card(style="margin: 20px 20px; font-size: 14px")
-          div(slot="header")
-            span 订单统计
-          el-row
-            el-col(:span="4")
-              div(style="padding: 20px")
-                div
-                  div(style="color: #909399;font-size: 14px") 本月订单总数
-                  div(style="color: #606266;font-size: 24px;padding: 10px 0") 10000
-                  div
-                    span.color-success(style="font-size: 14px") +10%
-                    span(style="color: #C0C4CC;font-size: 14px") 同比上月
-                div(style="margin-top: 20px;")
-                  div(style="color: #909399;font-size: 14px") 本周订单总数
-                  div(style="color: #606266;font-size: 24px;padding: 10px 0") 1000
-                  div
-                    span.color-danger(style="font-size: 14px") -10%
-                    span(style="color: #C0C4CC;font-size: 14px") 同比上周
-                div(style="margin-top: 20px;")
-                  div(style="color: #909399;font-size: 14px") 本月销售总额
-                  div(style="color: #606266;font-size: 24px;padding: 10px 0") 100000
-                  div
-                    span.color-success(style="font-size: 14px") +10%
-                    span(style="color: #C0C4CC;font-size: 14px") 同比上月
-                div(style="margin-top: 20px;")
-                  div(style="color: #909399;font-size: 14px") 本周销售总额
-                  div(style="color: #606266;font-size: 24px;padding: 10px 0") 50000
-                  div
-                    span.color-danger(style="font-size: 14px") -10%
-                    span(style="color: #C0C4CC;font-size: 14px") 同比上周
-            el-col(:span="20")
-              div(style="padding: 10px;border-left:1px solid #DCDFE6")
-                el-date-picker(
-                  v-model="orderCountDate"
-                  align="right"
-                  end-placeholder="结束日期"
-                  :picker-options="pickerOptions"
-                  range-separator="至"
-                  size="small"
-                  start-placeholder="开始日期"
-                  style="float: right;z-index: 1"
-                  type="daterange"
-                  unlink-panels
-                  @change="handleDateChange"
-                )
-                div
-                  ve-line(
-                    :data-empty="dataEmpty"
-                    :data="chartData"
-                    :legend-visible="false"
-                    :loading="loading"
-                    :settings="chartSettings"
-                  )
-      el-col(:span="8")
-        el-card(style="margin: 20px 20px; font-size: 14px")
-          div(slot="header")
-            span 发展历程
-          el-timeline
-            el-timeline-item(placement="top" timestamp="2018年")
-              el-card
-                h4 参与京东服务市场商品分析应用开发，参与京东服务市场会员积分应用开发
-            el-timeline-item(placement="top" timestamp="2019年")
-              el-card
-                h4 参与京东服务市场商品搬家应用开发，参与拼多多服务市场订单应用开发
-            el-timeline-item(placement="top" timestamp="2020年")
-              el-card
-                h4 所参与开发的拼多多订单应用排名服务市场类目第一，开始快手服务市场订单应用开发
-            el-timeline-item(placement="top" timestamp="2021年")
-              el-card
-                h4 日处理拼多多订单200万条，开始美团、饿了么应用市场应用开发
-            el-timeline-item(placement="top" timestamp="2022年")
-              el-card
-                h4 累计服务10万+电商平台店铺、5万+外卖店铺。开始抖音、淘宝服务市场订单应用开发，开源ruoyi-wms
+<template>
+  <div class="home-wrapper">
+    <el-card style="margin: 20px 20px; font-size: 14px">
+      <div slot="header"><span>工作台</span></div>
+      <div class="first"><img alt="" :src="avatar" style="width: 50px; height: 50px; float:left; margin-right:10px;border-radius:50%" />
+        <p style="font-size:16px;margin-bottom:8px">{{name}}，{{hello}}</p>
+        <p style="font-size:12px;color:rgb(185, 181, 189)">今天是：{{nowTime}}</p>
+      </div>
+      <div class="first">
+        <ul>
+          <li class="li" style="width: 33%">访客数
+            <el-popover class="ml5" content="功能开发中" placement="top" trigger="hover"><i class="el-icon-warning-outline" slot="reference"></i></el-popover>
+          </li>
+          <li class="li" style="width: 33%">支付买家数
+            <el-popover class="ml5" content="功能开发中" placement="top" trigger="hover"><i class="el-icon-warning-outline" slot="reference"></i></el-popover>
+          </li>
+          <li class="li" style="width: 33%">浏览量
+            <el-popover class="ml5" content="功能开发中" placement="top" trigger="hover"><i class="el-icon-warning-outline" slot="reference"></i></el-popover>
+          </li>
+        </ul>
+        <ul>
+          <li class="da" style="width: 33%">
+            <router-link to="/basic/warehouse">2,910</router-link>
+          </li>
+          <li class="da" style="width: 33%">
+            <router-link to="/basic/area">56</router-link>
+          </li>
+          <li class="da" style="width: 33%">
+            <router-link to="/basic/rack">21,085</router-link>
+          </li>
+        </ul>
+      </div>
+    </el-card>
+    <el-row class="pl20 pr20" :gutter="10">
+      <el-col :span="8">
+        <el-card shadow="always" style="padding-bottom: 20px">
+          <div slot="header"><span>售后</span></div>
+          <ul>
+            <li class="li" style="width: 50%">待处理
+              <el-popover class="ml5" content="功能开发中" placement="top" trigger="hover"><i class="el-icon-warning-outline" slot="reference"></i></el-popover>
+            </li>
+            <li class="li" style="width: 50%">处理中
+              <el-popover class="ml5" content="功能开发中" placement="top" trigger="hover"><i class="el-icon-warning-outline" slot="reference"></i></el-popover>
+            </li>
+          </ul>
+          <ul>
+            <li class="da" style="width: 50%">2</li>
+            <li class="da" style="width: 50%">1</li>
+          </ul>
+        </el-card>
+      </el-col>
+      <el-col :span="16">
+        <el-card shadow="always" style="padding-bottom: 20px">
+          <div slot="header"><span>订单</span></div>
+          <div>
+            <ul>
+              <li class="li">未发订单数
+                <el-popover class="ml5" content="功能开发中" placement="top" trigger="hover"><i class="el-icon-warning-outline" slot="reference"></i></el-popover>
+              </li>
+              <li class="li">未揽件有退款
+                <el-popover class="ml5" content="功能开发中" placement="top" trigger="hover"><i class="el-icon-warning-outline" slot="reference"></i></el-popover>
+              </li>
+              <li class="li">发货超时
+                <el-popover class="ml5" content="功能开发中" placement="top" trigger="hover"><i class="el-icon-warning-outline" slot="reference"></i></el-popover>
+              </li>
+              <li class="li">物流预警
+                <el-popover class="ml5" content="功能开发中" placement="top" trigger="hover"><i class="el-icon-warning-outline" slot="reference"></i></el-popover>
+              </li>
+            </ul>
+            <ul>
+              <li class="da">
+                <router-link to="/relation/customer">50</router-link>
+              </li>
+              <li class="da">
+                <router-link to="/relation/supplier">0</router-link>
+              </li>
+              <li class="da">
+                <router-link to="/relation/carrier">4</router-link>
+              </li>
+              <li class="da">
+                <router-link to="/item">3</router-link>
+              </li>
+            </ul>
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
+    <el-row class="pl20 pr20" :gutter="10">
+      <el-col :span="16">
+        <el-card style="margin: 20px 20px; font-size: 14px">
+          <div slot="header"><span>订单统计</span></div>
+          <el-row>
+            <el-col :span="4">
+              <div style="padding: 20px">
+                <div>
+                  <div style="color: #909399;font-size: 14px">本月订单总数</div>
+                  <div style="color: #606266;font-size: 24px;padding: 10px 0">10000</div>
+                  <div><span class="color-success" style="font-size: 14px">+10%</span><span style="color: #C0C4CC;font-size: 14px">同比上月</span></div>
+                </div>
+                <div style="margin-top: 20px;">
+                  <div style="color: #909399;font-size: 14px">本周订单总数</div>
+                  <div style="color: #606266;font-size: 24px;padding: 10px 0">1000</div>
+                  <div><span class="color-danger" style="font-size: 14px">-10%</span><span style="color: #C0C4CC;font-size: 14px">同比上周</span></div>
+                </div>
+                <div style="margin-top: 20px;">
+                  <div style="color: #909399;font-size: 14px">本月销售总额</div>
+                  <div style="color: #606266;font-size: 24px;padding: 10px 0">100000</div>
+                  <div><span class="color-success" style="font-size: 14px">+10%</span><span style="color: #C0C4CC;font-size: 14px">同比上月</span></div>
+                </div>
+                <div style="margin-top: 20px;">
+                  <div style="color: #909399;font-size: 14px">本周销售总额</div>
+                  <div style="color: #606266;font-size: 24px;padding: 10px 0">50000</div>
+                  <div><span class="color-danger" style="font-size: 14px">-10%</span><span style="color: #C0C4CC;font-size: 14px">同比上周</span></div>
+                </div>
+              </div>
+            </el-col>
+            <el-col :span="20">
+              <div style="padding: 10px;border-left:1px solid #DCDFE6">
+                <el-date-picker v-model="orderCountDate" align="right" end-placeholder="结束日期" :picker-options="pickerOptions" range-separator="至" size="small" start-placeholder="开始日期" style="float: right;z-index: 1" type="daterange" unlink-panels="unlink-panels" @change="handleDateChange"></el-date-picker>
+                <div>
+                  <ve-line :data-empty="dataEmpty" :data="chartData" :legend-visible="false" :loading="loading" :settings="chartSettings"></ve-line>
+                </div>
+              </div>
+            </el-col>
+          </el-row>
+        </el-card>
+      </el-col>
+      <el-col :span="8">
+        <el-card style="margin: 20px 20px; font-size: 14px">
+          <div slot="header"><span>发展历程</span></div>
+          <el-timeline>
+            <el-timeline-item placement="top" timestamp="2018年">
+              <el-card>
+                <h4>参与京东服务市场商品分析应用开发，参与京东服务市场会员积分应用开发</h4>
+              </el-card>
+            </el-timeline-item>
+            <el-timeline-item placement="top" timestamp="2019年">
+              <el-card>
+                <h4>参与京东服务市场商品搬家应用开发，参与拼多多服务市场订单应用开发</h4>
+              </el-card>
+            </el-timeline-item>
+            <el-timeline-item placement="top" timestamp="2020年">
+              <el-card>
+                <h4>所参与开发的拼多多订单应用排名服务市场类目第一，开始快手服务市场订单应用开发</h4>
+              </el-card>
+            </el-timeline-item>
+            <el-timeline-item placement="top" timestamp="2021年">
+              <el-card>
+                <h4>日处理拼多多订单200万条，开始美团、饿了么应用市场应用开发</h4>
+              </el-card>
+            </el-timeline-item>
+            <el-timeline-item placement="top" timestamp="2022年">
+              <el-card>
+                <h4>累计服务10万+电商平台店铺、5万+外卖店铺。开始抖音、淘宝服务市场订单应用开发，开源ruoyi-wms</h4>
+              </el-card>
+            </el-timeline-item>
+          </el-timeline>
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
 </template>
-
 <script>
 import {str2Date} from '@/utils/date';
 import PanelGroup from '@/views/components/PanelGroup'
--- src/views/pms/product/AddProduct.vue
+++ src/views/pms/product/AddProduct.vue
@@ -1,88 +1,106 @@
 <template>
   <div class="add-product-wrapper">
-    <el-form class="content" label-width="108px" :model="form" ref="form">
-      <el-form-item label="品牌" prop="brandId">
-        <brand-select v-model="form.brandId"></brand-select>
-      </el-form-item>
-      <el-form-item label="分类" prop="categoryId">
-        <product-category-select class="w200" v-model="form.categoryId"></product-category-select>
-      </el-form-item>
-      <el-form-item label="商品编码" prop="outProductId">
-        <el-input v-model="form.outProductId" placeholder="请输入商品编码"></el-input>
-      </el-form-item>
-      <el-form-item label="商品名称" prop="name">
-        <el-input v-model="form.name" placeholder="请输入商品名称"></el-input>
-      </el-form-item>
-      <el-form-item label="主图" prop="pic">
-        <image-upload v-model="form.pic" :limit="1"></image-upload>
-      </el-form-item>
-      <el-form-item label="商品图片" prop="albumPics">
-        <image-upload v-model="form.albumPics" :limit="5"></image-upload>
-      </el-form-item>
-      <el-form-item label="上架状态">
-        <dict-select v-model="form.publishStatus" prop-name="pms_publish_status"></dict-select>
-      </el-form-item>
-      <el-form-item label="排序" prop="sort">
-        <el-input v-model="form.sort" placeholder="请输入排序"></el-input>
-      </el-form-item>
-      <el-form-item label="价格" prop="price">
-        <el-input v-model="form.price" placeholder="请输入PRICE"></el-input>
-      </el-form-item>
-      <el-form-item label="单位" prop="unit">
-        <el-input v-model="form.unit" placeholder="请输入单位"></el-input>
-      </el-form-item>
-      <el-form-item label="商品重量" prop="weight">
-        <el-input v-model="form.weight" placeholder="商品重量，默认为克"></el-input>
-      </el-form-item>
-      <div class="title">规格信息</div>
-      <el-form-item label="商品规格">
-        <div class="sku-wrapper">
-          <div class="sku_sorts">
-            <div class="sku_sort" v-for="(s, idx0) in skuSorts" :key="s.name">
-              <div class="label flex-center">
-                <div class="flex-one">
-                  <dict-select v-model="s.name" prop-name="sku_sort_list" value-prop="label"></dict-select>
-                </div><a class="red" @click="deleteSkuSort(idx0)">删除规格类型</a>
-              </div>
-              <div class="values" v-if="s.name">
-                <div class="value" v-for="(it2, idx1) in s.options" :key="idx1">
-                  <el-input :value="it2.name" @input="changeName(s, idx1, $event)"></el-input><a class="red no-break ml8" v-if="idx1 &lt; s.options.length - 1 || (s.options.length === maxOptionNum &amp;&amp; idx1 === 3)" @click="deleteOption(s, idx1)">删除</a>
+    <el-form label-width="108px" :model="form" ref="form">
+      <el-card style="margin: 20px 20px; font-size: 14px">
+        <div slot="header">
+          <span>基本信息</span>
+        </div>
+        <el-form-item label="商品名称" prop="name">
+          <el-input v-model="form.name" placeholder="请输入商品名称"></el-input>
+        </el-form-item>
+        <el-form-item label="商品编码" prop="outProductId">
+          <el-input v-model="form.outProductId" placeholder="请输入商品编码"></el-input>
+        </el-form-item>
+        <el-form-item label="品牌" prop="brandId">
+          <brand-select v-model="form.brandId"></brand-select>
+        </el-form-item>
+        <el-form-item label="分类" prop="categoryId">
+          <product-category-select class="w200" v-model="form.categoryId"></product-category-select>
+        </el-form-item>
+        <el-form-item label="主图" prop="pic">
+          <oss-image-upload v-model="form.pic" :limit="1"></oss-image-upload>
+        </el-form-item>
+        <el-form-item label="轮播图" prop="albumPics">
+          <oss-image-upload v-model="form.albumPics" :limit="5"></oss-image-upload>
+        </el-form-item>
+        <el-form-item label="上架状态">
+          <DictRadio v-model="form.publishStatus" size="small"
+                    :radioData="dict.type.pms_publish_status"/>
+        </el-form-item>
+        <el-form-item label="排序" prop="sort">
+          <el-input v-model="form.sort" placeholder="请输入排序"></el-input>
+        </el-form-item>
+        <el-form-item label="价格" prop="price">
+          <el-input v-model="form.price" placeholder="请输入PRICE"></el-input>
+        </el-form-item>
+        <el-form-item label="单位" prop="unit">
+          <el-input v-model="form.unit" placeholder="请输入单位"></el-input>
+        </el-form-item>
+        <el-form-item label="商品重量" prop="weight">
+          <el-input v-model="form.weight" placeholder="商品重量，默认为克"></el-input>
+        </el-form-item>
+      </el-card>
+      
+      <el-card style="margin: 20px 20px; font-size: 14px">
+        <div slot="header">
+          <span>规格信息</span>
+        </div>
+        <el-form-item label="规格类型">
+          <div class="sku-wrapper">
+            <div class="sku_sorts">
+              <div class="sku_sort" v-for="(s, idx0) in skuSorts" :key="s.name">
+                <div class="label flex-center">
+                  <div class="flex-one">
+                    <dict-select v-model="s.name" prop-name="sku_sort_list" value-prop="label"></dict-select>
+                  </div><a class="red" @click="deleteSkuSort(idx0)">删除规格类型</a>
+                </div>
+                <div class="values" v-if="s.name">
+                  <div class="value" v-for="(it2, idx1) in s.options" :key="idx1">
+                    <el-input :value="it2.name" @input="changeName(s, idx1, $event)" placeholder="请输入规格名称"></el-input><a class="red no-break ml8" v-if="idx1 &lt; s.options.length - 1 || (s.options.length === maxOptionNum &amp;&amp; idx1 === 3)" @click="deleteOption(s, idx1)">删除</a>
+                  </div>
                 </div>
               </div>
             </div>
+            <el-button v-if="skuSorts.length &lt; 2" @click="addSkuSort">+添加规格类型</el-button>
           </div>
-          <el-button v-if="skuSorts.length &lt; 2" @click="addSkuSort">添加规格类型</el-button>
+        </el-form-item>
+        <el-form-item label="规格信息">
+          <el-table :data="skuList" :max-height="400">
+            <el-table-column v-for="s in skuSorts" :label="s.name" :key="s.name" :prop="s.name"></el-table-column>
+            <el-table-column label="展示图片">
+              <template v-slot="{ row }">
+                <oss-image-upload class="img-upload-mini" v-model="row.pic" :is-show-tip="false"></oss-image-upload>
+              </template>
+            </el-table-column>
+            <el-table-column label="销售价格">
+              <template v-slot="{ row }">
+                <el-input v-model="row.price"></el-input>
+              </template>
+            </el-table-column>
+            <el-table-column label="编码">
+              <template v-slot="{ row }">
+                <el-input v-model="row.outSkuId"></el-input>
+                <el-input v-model="row.spData" v-show="false"></el-input>
+              </template>
+            </el-table-column>
+            <el-table-column label="操作">
+              <template v-slot="{row, index}"><a class="red">删除{{index}}</a></template>
+            </el-table-column>
+          </el-table>
+        </el-form-item>
+      </el-card>
+      <el-card style="margin: 20px 20px; font-size: 14px">
+        <div slot="header">
+          <span>详情页</span>
         </div>
-      </el-form-item>
-      <el-form-item label=" 价格">
-        <el-table :data="skus" :max-height="400">
-          <el-table-column v-for="s in skuSorts" :label="s.name" :key="s.name" :prop="s.name"></el-table-column>
-          <el-table-column label="展示图片">
-            <template v-slot="{ row }">
-              <image-upload class="img-upload-mini" v-model="row.pic" :is-show-tip="false"></image-upload>
-            </template>
-          </el-table-column>
-          <el-table-column label="销售价格">
-            <template v-slot="{ row }">
-              <el-input v-model="row.price"></el-input>
-            </template>
-          </el-table-column>
-          <el-table-column label="编码">
-            <template v-slot="{ row }">
-              <el-input v-model="row.outSkuId"></el-input>
-            </template>
-          </el-table-column>
-          <el-table-column label="操作">
-            <template v-slot="{row, index}"><a class="red">删除{{index}}</a></template>
-          </el-table-column>
-        </el-table>
-      </el-form-item>
-      <el-form-item label="产品详情网页内容" prop="detailHtml">
-        <el-input v-model="form.detailHtml" placeholder="请输入内容" type="textarea"></el-input>
-      </el-form-item>
-      <el-form-item label="移动端网页详情" prop="detailMobileHtml">
-        <el-input v-model="form.detailMobileHtml" placeholder="请输入内容" type="textarea"></el-input>
-      </el-form-item>
+        <el-form-item label="产品详情网页内容" prop="detailHtml">
+          <el-input v-model="form.detailHtml" placeholder="请输入内容" type="textarea"></el-input>
+        </el-form-item>
+        <el-form-item label="移动端网页详情" prop="detailMobileHtml">
+          <el-input v-model="form.detailMobileHtml" placeholder="请输入内容" type="textarea"></el-input>
+        </el-form-item>
+      </el-card>
+      
       <div class="tc">
         <el-button type="primary" @click="submitForm">确 定</el-button>
         <el-button @click="cancel">取 消</el-button>
@@ -98,6 +116,7 @@ import BrandSelect from "@/views/components/BrandSelect";
 
 export default {
   name: "AddProduct",
+  dicts: ['pms_publish_status'],
   components: {BrandSelect, ProductCategorySelect},
   data() {
     return {
@@ -115,29 +134,36 @@ export default {
     }
   },
   computed: {
-    skus() {
+    skuList() {
       let arr = [];
-      this.skuSorts.forEach((it1, idx1) => {
+      this.skuSorts.forEach((sku, index) => {
         const arr1 = [];
-        it1.options.forEach((it2) => {
-          if (!it2.name) {
+        sku.options.forEach((option) => {
+          if (!option.name) {
             return
           }
-          if (idx1 === 0) {
-            arr1.push({[it1.name]: it2.name});
+          if (index === 0) {
+            arr1.push({[sku.name]: option.name});
           } else {
             arr.forEach(it3 => {
-              arr1.push({...it3, [it1.name]: it2.name })
+              arr1.push({...it3, [sku.name]: option.name })
             })
           }
         })
         arr = arr1;
       })
       arr.forEach(it => {
+        if(it){
+          it.spData=JSON.stringify(it)
+        }
+        
+      })
+      arr.forEach(it => {
         it.outSkuId = null;
         it.price = null;
         it.pic = null;
       })
+      console.log(arr)
       return arr
     }
   },
@@ -161,17 +187,18 @@ export default {
     submitForm() {
       this.$refs["form"].validate(valid => {
         if (valid) {
+          this.form.skuList = this.skuList
+          if(this.form.categoryId){
+            this.form.categoryId = this.form.categoryId.pop()
+          }
           if (this.form.id != null) {
             updatePmsProduct(this.form).then(response => {
               this.$modal.msgSuccess("修改成功");
-              this.open = false;
-              this.getList();
             });
           } else {
             addPmsProduct(this.form).then(response => {
+              console.log(this.form)
               this.$modal.msgSuccess("新增成功");
-              this.open = false;
-              this.getList();
             });
           }
         }
